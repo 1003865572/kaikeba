5bc57e1b4ea9cfe4125da2c9272a4fdabc7a4f8e
石兴隆 <1003865572@qq.com>
2018-01-14 15:44:14 +0800

    2018-1-14

---
+++ 0sxl/2018-1-13/babel/build/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Person = function Person(name, age) {
+  _classCallCheck(this, Person);
+
+  this.name = name;
+  this.age = age;
+};
+
+var p = new Person('blue');
+console.log(p);
---
+++ 0sxl/2018-1-13/babel/js/index.js
@@ -0,0 +1,9 @@
+
+  class Person {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+  }
+  let p = new Person('blue')
+  console.log(p)
