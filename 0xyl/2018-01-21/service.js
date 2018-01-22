1c8b793eae57f547d47ab32a73da49ca5866d08c
暮色听雨声 <425324438@qq.com>
2018-01-23 00:10:50 +0800

    u

--- 0xyl/2018-01-21/service.js
+++ 0xyl/2018-01-21/service.js
@@ -1,17 +1,23 @@
 const http = require('http');
+
 const fs = require('fs');
 
 let service = http.createServer((res,rep)=>{
-    fs.readFile('1.html',(err,data)=>{
-        if(err){
-            console.info(err);    
-            rep.write(err);
-        }else{
-            console.info('请求正确');
-            rep.write(data);
-        }
-        rep.end();
-    });
+
+    fs.writeFile('1.text','asdasdsd',()=>{
+      console.log(this);
+    })
+
+    // fs.readFile('1.html',(err,data)=>{
+    //     if(err){
+    //         console.info(err);    
+    //         rep.write(err);
+    //     }else{
+    //         console.info('请求正确');
+    //         rep.write(data);
+    //     }
+    //     rep.end();
+    // });
 });
 
 service.listen(8081)
