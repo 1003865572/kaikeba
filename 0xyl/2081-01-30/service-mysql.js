8e6d99c610f64b25195f3d83ecb81362449d78a9
暮色听雨声 <425324438@qq.com>
2018-02-03 19:56:34 +0800

    up

--- 0xyl/2081-01-30/service-mysql.js
+++ 0xyl/2081-01-30/service-mysql.js
@@ -4,35 +4,43 @@ const mysql = require('mysql');
 // const crypot = require('crypot');
 const zlib = require('zlib');
 const fs = require('fs');
-
+let gz = zlib.createGzip();
 let db = mysql.createPool({host:'127.0.0.1', prot : '3306', user :'root',password:'',database:'test'});
 
 let service = http.createServer((req,rep)=>{
     
+    try{
+
     let {pathname,query} = url.parse(req.url,true);
     let {user,pass} = query;
 
     console.log(pathname);
     
     switch(pathname){
-        case '/1.html':
-            let file = fs.createReadStream(`www/${pathname}`);
-            let gz = zlib.createGzip();
-            rep.setHeader('content-encoding','gzip');
-            file.pipe(gz).pipe(rep);
-            break;
         case '/req':
             console.log(user,pass);
             break;
         case '/login':
             console.log(user,pass);
             break;
-        default :
-            rep.writeHead('404');
-            rep.writeHead('Not Found');
-            rep.end();
-            break;
-
     }
+
+
+        let files = fs.createReadStream(`www/${pathname}`);
+        rep.setHeader('content-encoding', 'gzip');
+    
+        files.pipe(gz).pipe(rep);
+        
+        files.on('error',err=>{
+            console.log(`文件流error${err}`);
+            rep.writeHead(404);
+            rep.write('Not Found');
+            rep.end();
+        });
+    }catch (e){
+        console.log(`catch捕获到的异常：${e}`);
+    }
+
+    
 });
 service.listen(8080);
