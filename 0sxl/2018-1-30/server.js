c3e206a9dcb8247e2896db48fafd54d30963b0d8
[1003865572@qq.com] <1003865572@qq.com>
2018-03-06 21:06:36 +0800

    2-18-3-3

--- 0sxl/2018-1-30/server.js
+++ 0sxl/2018-1-30/server.js
@@ -1,1 +1,34 @@
 const mysql = require('mysql');
+const fs = require('fs');
+const http = require('http');
+const zlib = require('zlib');
+const urlLib = require('url');
+
+
+let httpServer = http.createServer((req, res) => {
+    const {pathname, query} = urlLib.parse(req.url, true);
+    const {user, pass} = query;
+    switch (pathname) {
+        case '/reg' : 
+            console.log('注册')
+        break;
+        case '/login' : 
+            console.log('登录')
+        break;
+        default: 
+            let rs = fs.createReadStream(`www${pathname}`);
+            let gz = zlib.createGzip();
+            res.setHeader('content-encoding', 'gzip')
+            rs.pipe(gz).pipe(res);
+            
+            rs.on('error', () => {
+                console.log(pathname);
+                res.writeHeader(404);
+                res.write('Not Found');
+                res.end();
+            })
+    }
+})
+
+httpServer.listen(8888);
+console.log('server runing is ok prot 8888');
