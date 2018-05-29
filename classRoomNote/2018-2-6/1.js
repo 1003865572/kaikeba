478e8b9e50e8a176a2784fdd1d9d0f2d03a78c7c
425324438 <a1003865572>
2018-05-29 22:26:26 +0800

    up

--- classRoomNote/2018-2-6/1.js
+++ classRoomNote/2018-2-6/1.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const http=require('http');
 const fs=require('fs');
 
@@ -6,12 +5,3 @@ http.createServer((req, res)=>{
   res.end();
   res.write('aaa');
 }).listen(8080);
-=======
-const http=require('http');
-const fs=require('fs');
-
-http.createServer((req, res)=>{
-  res.end();
-  res.write('aaa');
-}).listen(8080);
->>>>>>> 3a8e24d65a75c6e257e09d9fb72582456df2b792
