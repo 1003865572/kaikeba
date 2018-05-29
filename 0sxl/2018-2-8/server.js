936471b5ed0821be4b17083a9c4656d0788226f9
1003865572 <1003865572@qq.com>
2018-05-29 23:40:17 +0800

    5-29

--- 0sxl/2018-2-8/server.js
+++ 0sxl/2018-2-8/server.js
@@ -11,15 +11,12 @@ server.listen(8080);
 server.use(bodyPasser.urlencoded({ extended: false }));
 server.use(upload.any());
 
-server.post('/api', (req, res) => {
+server.use('/api', (req, res) => {
+    console.log(req.method, req.url);
     res.setHeader('Access-Control-Allow-Origin', '*');
-    // if(req.headers['origin']=='null' || req.headers['origin'].startsWith('http://localhost')){
-    //     res.setHeader('Access-Control-Allow-Origin', '*');
-    // }
-    console.log(req.body);
     console.log(req.files);
-    res.body='111'
-    this.body = '111';
+    
+    res.end('ok');
 })
 
 server.use(express.static('./www'));
--- classRoomNote/2018-2-8/server.js
+++ classRoomNote/2018-2-8/server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const http=require('http');
 const fs=require('fs');
 const zlib=require('zlib');
@@ -20,26 +19,3 @@ let server=http.createServer((req, res)=>{
   });
 });
 server.listen(8080);
-=======
-const http=require('http');
-const fs=require('fs');
-const zlib=require('zlib');
-
-let server=http.createServer((req, res)=>{
-  let rs=fs.createReadStream('./www'+req.url);
-  let gz=zlib.createGzip();
-
-  //asdfasdfasfasdfasd
-
-  res.setHeader('Content-Encoding', 'gzip');
-  rs.pipe(gz).pipe(res);
-
-  rs.on('error', (err)=>{
-    res.setHeader('Content-Encoding', '');
-    res.writeHeader(404);
-    res.write('Not Found');
-    res.end();
-  });
-});
-server.listen(8080);
->>>>>>> 3a8e24d65a75c6e257e09d9fb72582456df2b792
