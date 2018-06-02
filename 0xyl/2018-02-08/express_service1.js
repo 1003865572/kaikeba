4dad17ebadd13dd0547b07c758791837682775f1
425324438 <a1003865572>
2018-06-03 00:45:21 +0800

    up

--- 0xyl/2018-02-08/express_service1.js
+++ 0xyl/2018-02-08/express_service1.js
@@ -12,8 +12,10 @@ let multerObj = multer({dest : './update'});
 server.use(multerObj.any());
 
 server.use('/api',(req,rep)=>{
+    console.log('请求进入' + req.headers['origin']);
     if(req.headers['origin'] == null || req.headers['origin'].startsWith('http://localhost')){
         rep.setHeader('Access-Control-Allow-Origin','*');
+        console.log('跨域访问进来的');
     }
 
     rep.send('ok');
