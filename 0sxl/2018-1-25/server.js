e942fc2ebc5ee1f915faf0f0230377ed23b1787f
1003865572 <1003865572@qq.com>
2018-06-24 14:35:58 +0800

    6-24

--- 0sxl/2018-1-25/server.js
+++ 0sxl/2018-1-25/server.js
@@ -3,22 +3,81 @@ const common = require('./common')
 
 let server = http.createServer((req, res) => {
     let arr = []
+
+    res.setHeader('Access-Control-Allow-Origin', '*')
+
     req.on('data', data => arr.push(data));
     req.on('end', () => {
+        res.write('over');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         let data = Buffer.concat(arr);
-        // data
+        // data 
         let str = req.headers['content-type'];
         if (str && str.indexOf('boundary') !== -1) {
             str = str.split('; ')[1];
-            console.log(str)
+            let boundary = `--${str.split('=')[1]}`;
+
+            let resultArr = data.split(boundary);
+            resultArr.shift();
+            resultArr.pop()
+            resultArr = resultArr.map(buffer => buffer.slice(2, buffer.length))
+            resultArr.map(b => {
+                console.log(b.toString());
+            })
+            // let resultArr1 = resultArr.map(item => {
+            //     // item
+            //     return item.split('Content-Disposition: form-data; ')[1]
+            // })
+
+
+
+            // let resultArr3 = {}
+            // resultArr1.map(item => {
+            //     console.log(item.toString())
+            //     if (item.indexOf('Content-Type') !== -1) {
+            //         // item.split(/\r\n/g).map(n => {
+            //         //     console.log(n.toString());
+            //         // })
+            //         // resultArr3.push({
+            //         //     name: '',
+            //         //     filename: '',
+            //         //     data: []
+            //         // })
+            //         // resultArr3
+            //     } else {
+            //         // resultArr3
+            //     }
+            // })
         }
-        // let str = req.headers['content-type'].split('; ')[1]
-        // if (str) {
-        //     console.log(str);
-        // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        res.end();
     })
     
-    res.end();
 })
 
 server.listen(8080);
